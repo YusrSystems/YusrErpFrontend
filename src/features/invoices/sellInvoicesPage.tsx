794a68f236a2f3f84f89458ea35163ee4516b143
Ahmed Bakri <AhmedBkry204@gmail.com>
2026-04-12 20:48:51 +0300

    update- state to fetch invoice by id

--- src/features/invoices/sellInvoicesPage.tsx
+++ src/features/invoices/sellInvoicesPage.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { InvoiceType } from "../../core/data/invoice";
 import InvoicesPage from "./invoicesPage";
 
 export default function SellInvoicesPage()
 {
+  const { invoiceId } = useParams();
+
+  useEffect(() =>
+  {
+    if (invoiceId)
+    {
+      // openInvoiceDialog(invoiceId);
+    }
+  }, [invoiceId]);
   return <InvoicesPage type={ InvoiceType.Sell } />;
 }
